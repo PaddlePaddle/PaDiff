e0409117ed60281e884cdbbf9550ab786ae72f07
feifei-111 <2364819892@qq.com>
2023-01-16 07:39:48 +0000

    add test

--- padiff/utils.py
+++ padiff/utils.py
@@ -362,18 +362,18 @@ def assert_tensor_equal(tensor1, tensor2, atol=0, rtol=1e-7, compare_mode="mean"
 
 
 def init_options(options):
-    if "atol" not in options.keys():
-        options["atol"] = 0
-    if "rtol" not in options.keys():
-        options["rtol"] = 1e-7
-    if "diff_phase" not in options.keys():
-        options["diff_phase"] = "both"
-    if "compare_mode" not in options.keys():
-        options["compare_mode"] = "mean"
-    if "single_step" not in options.keys():
-        options["single_step"] = False
-    if "debug" not in options.keys():
-        options["debug"] = False
+    default_options = {
+        "atol": 0,
+        "rtol": 1e-7,
+        "diff_phase": "both",
+        "compare_mode": "mean",
+        "single_step": False,
+        "debug": False,
+    }
+
+    for key in default_options.keys():
+        if key not in options.keys():
+            options[key] = default_options[key]
 
     if options["single_step"]:
         options["diff_phase"] = "forward"
@@ -381,9 +381,6 @@ def init_options(options):
 
     log("Your options:")
     print("{")
-    print("  atol : `{}`".format(options["atol"]))
-    print("  rtol : `{}`".format(options["rtol"]))
-    print("  diff_phase : `{}`".format(options["diff_phase"]))
-    print("  compare_mode : `{}`".format(options["compare_mode"]))
-    print("  single_step : `{}`".format(options["single_step"]))
+    for key in options.keys():
+        print("  {}: `{}`".format(key, options[key]))
     print("}")
---
+++ tests/test_diff_phase.py
@@ -0,0 +1,81 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+
+import paddle
+import torch
+
+from padiff import auto_diff
+
+
+class SimpleLayer(paddle.nn.Layer):
+    def __init__(self):
+        super(SimpleLayer, self).__init__()
+        self.linear1 = paddle.nn.Linear(100, 100)
+        self.linear2 = paddle.nn.Linear(100, 10)
+        self.act = paddle.nn.ReLU()
+
+    def forward(self, x):
+        """
+        x -> linear1 -> x -> relu -> x -> add -> linear2 -> output
+        |                                  |
+        |----------------------------------|
+        """
+        resdual = x
+        x = self.linear1(x)
+        x = self.act(x)
+        x = x + resdual
+        x = self.linear2(x)
+        return x
+
+
+class SimpleModule(torch.nn.Module):
+    def __init__(self):
+        super(SimpleModule, self).__init__()
+        self.linear1 = torch.nn.Linear(100, 100)
+        self.linear2 = torch.nn.Linear(100, 10)
+        self.act = torch.nn.ReLU()
+
+    def forward(self, x):
+        """
+        x -> linear1 -> x -> relu -> x -> add -> linear2 -> output
+        |                                  |
+        |----------------------------------|
+        """
+        resdual = x
+        x = self.linear1(x)
+        x = self.act(x)
+        x = x + resdual
+        x = self.linear2(x)
+        return x
+
+
+class TestCaseName(unittest.TestCase):
+    def test_check_weight_grad(self):
+        layer = SimpleLayer()
+        module = SimpleModule()
+        options = {"atol": 1e-4, "diff_phase": "forward"}
+
+        inp = paddle.rand((100, 100)).numpy().astype("float32")
+        inp = ({"x": paddle.to_tensor(inp)}, {"x": torch.as_tensor(inp)})
+        assert auto_diff(layer, module, inp, auto_weights=True, options=options) is True, "Failed. expected success."
+
+        for param in module.parameters():
+            assert param.grad is None
+
+
+if __name__ == "__main__":
+    unittest.main()
