c2da8f306ae64cd51d919db1a651cb3ba4e1a816
feifei-111 <2364819892@qq.com>
2023-02-15 06:50:11 +0000

    import ok, add getattr later

--- padiff/__init__.py
+++ padiff/__init__.py
@@ -57,7 +57,9 @@ class PaDiffFinder(MetaPathFinder):
         found, module_type = self.module_filter(fullname)
         if found:
             spec = self.sys_find_spec(fullname, path, target)
-            if spec is not None and not isinstance(spec.loader, ExtensionFileLoader):
+            # ExtensionFileLoader loader .so and other lib file
+            # if loader is None, python may use a _NamespaceLoader or other way to init
+            if spec is not None and not isinstance(spec.loader, (ExtensionFileLoader)) and spec.loader is not None:
                 spec._module_type = module_type
                 spec.loader = PaDiffLoader(spec.loader)
             return spec
@@ -67,7 +69,6 @@ class PaDiffFinder(MetaPathFinder):
         for finder in sys.meta_path:
             if isinstance(finder, PaDiffFinder):
                 continue
-
             try:
                 find_spec = finder.find_spec
             except AttributeError:
@@ -84,141 +85,118 @@ class PaDiffLoader(Loader):
         self._loader = _loader
 
     def exec_module(self, module):
-        self._loader.exec_module(module._real)
-        module._importing = False
+        self._loader.exec_module(module)
+
+        # module._module_type = module.__spec__._module_type
+        # module._wrapped_cache = {}
+        # module._in_api_flag = False
+
+        # try:
+        #     get_method = self.__getattribute__
+        #     self.__origin_getattribute__ = get_method
+        #     self.__getattribute__ = __my_getattribute__
+        # except:
+        #     print(str(module) + "getattribute set failed")
 
     def create_module(self, spec):
-        return PaDiffModule(spec)
+        # return PaDiffModule(spec)
+        return None
 
 
 class PaDiffModule(ModuleType):
     def __init__(self, spec):
-        self._real = ModuleType(spec.name)
-        if hasattr(spec, "loader"):
-            self._real.__loader__ = spec.loader
-        if hasattr(spec, "parent"):
-            self._real.__package__ = spec.parent
-        self._real.__spec__ = spec
-        if hasattr(spec, "submodule_search_locations"):
-            self._real.__path__ = spec.submodule_search_locations
-        if hasattr(spec, "origin"):
-            self._real.__file__ = spec.origin
-        if hasattr(spec, "cached"):
-            self._real.__cached__ = spec.cached
-
         self._module_type = spec._module_type
         self._wrapped_cache = {}
         self._in_api_flag = False
         self._importing = True
 
-    def __setattr__(self, name, value):
-        if name not in ["_module_type", "_wrapped_cache", "_in_api_flag", "_real", "_importing"]:
-            setattr(self._real, name, value)
-        else:
-            super(PaDiffModule, self).__setattr__(name, value)
 
-    def __getattr__(self, name):
-        if name in self._wrapped_cache.keys():
-            return self._wrapped_cache[name]
+def __my_getattribute__(self, name):
+    if name in self._wrapped_cache.keys():
+        return self._wrapped_cache[name]
 
-        try:
-            obj = self._real.__getattribute__(name)
-        except:
-            try:
-                return self._real.__getattr__(name, None)
-            except Exception:
-                # '__all__' program, use _importing to fix
-                if self.__dict__["_importing"] == True or name not in self.__dict__.keys():
-                    raise AttributeError(f"{name} is not found in {self._real.__name__}")
-                else:
-                    return self.__dict__.keys()
+    try:
+        obj = self._orig_getattribute(name)
+    except:
+        obj = self.__dict__[name]
 
-        if inspect.ismodule(obj):
+    if inspect.ismodule(obj):
+        return obj
+
+    # a function, and not in api
+    if not self._in_api_flag and inspect.isfunction(obj):
+        if name in SKIP_NAMES.keys() and SKIP_NAMES[name] == self._real.__name__:
+            self._wrapped_cache[name] = obj
             return obj
 
-        # a function, and not in api
-        if not self._in_api_flag and inspect.isfunction(obj):
-            if name in SKIP_NAMES.keys() and SKIP_NAMES[name] == self._real.__name__:
-                self._wrapped_cache[name] = obj
-                return obj
+        # only when this api is called, a Layer/Module is built
+        def wrapped(*args, **kwargs):
+            self._in_api_flag = True
 
-            # if import is not finished, do not return wrapped
-            if self._real.__name__ != "paddle" and self._real.__name__ != "torch":
-                if self._module_type == "paddle":
-                    if "paddle" not in sys.modules.keys() or sys.modules["paddle"]._importing is True:
-                        return obj
-                else:
-                    if "torch" not in sys.modules.keys() or sys.modules["torch"]._importing is True:
-                        return obj
+            out = obj(*args, **kwargs)
 
-            # only when this api is called, a Layer/Module is built
-            def wrapped(*args, **kwargs):
-                self._in_api_flag = True
+            import paddle, torch
 
-                out = obj(*args, **kwargs)
-
-                import paddle, torch
-
-                # from paddle.fluid.layers.utils import flatten
-                # only transform apis which ret Tensor
-                if out is None or all(
-                    [not isinstance(x, (paddle.Tensor, torch.Tensor)) for x in paddle.fluid.layers.utils.flatten(out)]
-                ):
-                    self._in_api_flag = False
-                    return out
-
-                # get Layer and register hook
-                if self._module_type == "paddle":
-
-                    from .Trainer import paddle_layer_hook
-
-                    class PaddleApi(paddle.nn.Layer):
-                        def __init__(self, func):
-                            super(PaddleApi, self).__init__()
-                            self._func = func
-                            self._name = name
-
-                        def forward(self, *args, **kwargs):
-                            return self._func(*args, **kwargs)
-
-                    layer = PaddleApi(obj)
-                    # need idx to support single step, set idx -1 here to skip api in single step mode
-                    handle = layer.register_forward_post_hook(partial(paddle_layer_hook, idx=-1))
-
-                elif self._module_type == "torch":
-
-                    from .Trainer import torch_layer_hook
-
-                    class TorchApi(torch.nn.Module):
-                        def __init__(self, func):
-                            super(TorchApi, self).__init__()
-                            self.func = func
-                            self._name = name
-
-                        def forward(self, *args, **kwargs):
-                            return self.func(*args, **kwargs)
-
-                    layer = TorchApi(obj)
-                    handle = layer.register_forward_hook(partial(torch_layer_hook, idx=-1))
-
-                else:
-                    raise RuntimeError("Import Err: module_type not in (paddle, torch)")
-
-                # call forward
-                sys.stdout = open(os.devnull, "w")
-                out = layer(*args, **kwargs)
-                sys.stdout = sys.__stdout__
-
-                # remove hook
-                handle.remove()
-
+            # from paddle.fluid.layers.utils import flatten
+            # only transform apis which ret Tensor
+            if out is None or all(
+                [not isinstance(x, (paddle.Tensor, torch.Tensor)) for x in paddle.fluid.layers.utils.flatten(out)]
+            ):
                 self._in_api_flag = False
                 return out
 
-            self._wrapped_cache[name] = wrapped
-            return self._wrapped_cache[name]
+            # get Layer and register hook
+            if self._module_type == "paddle":
 
-        return obj
+                from .Trainer import paddle_layer_hook
+
+                class PaddleApi(paddle.nn.Layer):
+                    def __init__(self, func):
+                        super(PaddleApi, self).__init__()
+                        self._func = func
+                        self._name = name
+
+                    def forward(self, *args, **kwargs):
+                        return self._func(*args, **kwargs)
+
+                layer = PaddleApi(obj)
+                # need idx to support single step, set idx -1 here to skip api in single step mode
+                handle = layer.register_forward_post_hook(partial(paddle_layer_hook, idx=-1))
+
+            elif self._module_type == "torch":
+
+                from .Trainer import torch_layer_hook
+
+                class TorchApi(torch.nn.Module):
+                    def __init__(self, func):
+                        super(TorchApi, self).__init__()
+                        self.func = func
+                        self._name = name
+
+                    def forward(self, *args, **kwargs):
+                        return self.func(*args, **kwargs)
+
+                layer = TorchApi(obj)
+                handle = layer.register_forward_hook(partial(torch_layer_hook, idx=-1))
+
+            else:
+                raise RuntimeError("Import Err: module_type not in (paddle, torch)")
+
+            # call forward
+            sys.stdout = open(os.devnull, "w")
+            out = layer(*args, **kwargs)
+            sys.stdout = sys.__stdout__
+
+            # remove hook
+            handle.remove()
+
+            self._in_api_flag = False
+            return out
+
+        self._wrapped_cache[name] = wrapped
+        return self._wrapped_cache[name]
+
+    return obj
 
 
 sys.meta_path = [PaDiffFinder()] + sys.meta_path
