20726cc76b61238b215a43249d3ac5345c2a7298
feifei-111 <2364819892@qq.com>
2023-03-06 12:04:23 +0000

    update

--- padiff/utils.py
+++ padiff/utils.py
@@ -315,6 +315,8 @@ def log(*args):
 def max_diff(paddle_output, torch_output):
     _max_diff = 0
     for (pt,), (tt,) in zip(for_each_tensor(paddle_output), for_each_tensor(torch_output)):
+        if tt.numel() == 0 or pt.numel() == 0:
+            continue
         temp = np.abs(tt.detach().cpu().numpy() - pt.detach().numpy()).max()
         if temp > _max_diff:
             _max_diff = temp
