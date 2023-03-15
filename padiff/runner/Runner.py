255b46b48b45be62601769c532ec7c0a338bcd9c
feifei-111 <2364819892@qq.com>
2023-03-15 12:56:00 +0000

    finish

---
+++ padiff/runner/Runner.py
@@ -0,0 +1,95 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .Trainer import Trainer
+from .OptimizerHelper import OptimizerHelper
+from .Checker import Checker
+
+from .runner_utils import Report
+from ..utils import log
+
+
+class Runner:
+    def __init__(self, layer, module, loss_fn, opt, layer_map, options):
+        self.trainer = Trainer(layer, module, loss_fn, layer_map, options)
+        self.optimizer_helper = OptimizerHelper(opt, options)
+        self.options = options
+        self.steps = options["steps"]
+        self.layer_map = layer_map
+
+    def do_train(self, paddle_report, torch_report, example_inp):
+        self.trainer.set_report(paddle_report, torch_report)
+        self.trainer.train_step(example_inp)
+
+    def do_check(self, paddle_report, torch_report):
+        ret = Checker.check_forward_and_backward(torch_report, paddle_report, self.options)
+        if ret == False:
+            return ret
+
+        weight_check, grad_check = Checker.weight_grad_check(
+            self.trainer.layer, self.trainer.module, options=self.options, layer_map=self.layer_map
+        )
+        ret = weight_check and grad_check
+        return ret
+
+    def do_optimizer(self):
+        self.optimizer_helper.step()
+
+    def Run(self, example_inp):
+        if self.options["single_step"]:
+            return self.run_single_step(example_inp)
+        else:
+            return self.run_normal(example_inp)
+
+    def run_normal(self, example_inp):
+        for step_id in range(self.options["steps"]):
+            log(f"=================Train Step {step_id}=================")
+            paddle_report = Report("paddle")
+            torch_report = Report("torch")
+            self.do_train(paddle_report, torch_report, example_inp)
+            self.do_optimizer()
+            ret = self.do_check(paddle_report, torch_report)
+            if ret == False:
+                return False
+        return True
+
+    def run_single_step(self, example_inp):
+        diff_phase = self.options["diff_phase"]
+        for step_id in range(self.options["steps"]):
+            log(f"=================Train Step {step_id}=================")
+            if diff_phase == "forward" or diff_phase == "both":
+                log(f"diff phase is {diff_phase}, run single_step forward part.")
+                self.options["diff_phase"] = "forward"
+                paddle_report = Report("paddle")
+                torch_report = Report("torch")
+                self.do_train(paddle_report, torch_report, example_inp)
+                ret = self.do_check(paddle_report, torch_report)
+                if ret == False:
+                    log("Diff found at sinle_step mode `forward` part!")
+                    return False
+
+            if diff_phase == "backward" or diff_phase == "both":
+                log(f"diff phase is {diff_phase}, run single_step backward part.")
+                self.options["diff_phase"] = "backward"
+                paddle_report = Report("paddle")
+                torch_report = Report("torch")
+                self.do_train(paddle_report, torch_report, example_inp)
+                self.do_optimizer()
+                ret = self.do_check(paddle_report, torch_report)
+                if ret == False:
+                    log("Diff found at sinle_step mode `backward` part!")
+                    return False
+
+        self.options["diff_phase"] = diff_phase
+        return True
---
+++ padiff/runner/Trainer.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .runner_utils import report_guard, register_paddle_hooker, register_torch_hooker
+from ..utils import (
+    log,
+    max_diff,
+    tensors_mean,
+)
+from ..weights import remove_inplace, assign_weight
+
+
+class Trainer(object):
+    def __init__(self, layer, module, loss_fn, layer_map, options):
+        # self.paddle_device = paddle.get_device()
+        # self.torch_device = next(module.parameters()).device
+
+        self.layer = layer  # paddle layer
+        self.module = module  # torch module
+        self.options = options
+
+        if options["use_loss"]:
+            self.paddle_loss = loss_fn[0]
+            self.torch_loss = loss_fn[1]
+
+        # layer_map should be part of the module
+        self.layer_map = layer_map
+
+        remove_inplace(self.layer, self.module)
+
+        self.paddle_rep = None
+        self.torch_rep = None
+
+    def assign_weight_(self):
+        return assign_weight(self.layer, self.module, self.layer_map)
+
+    def set_report(self, paddle_rep, torch_rep):
+        paddle_rep.layer_map = self.layer_map
+        torch_rep.layer_map = self.layer_map
+        self.paddle_rep = paddle_rep
+        self.torch_rep = torch_rep
+
+    def train_step(self, example_inp):
+        paddle_input, torch_input = example_inp
+        with report_guard(self.torch_rep, self.paddle_rep):
+            with register_torch_hooker(self.module, self.layer_map):
+                try:
+                    torch_output = self.module(**torch_input)
+                    if self.options["use_loss"]:
+                        loss = self.torch_loss(torch_output)
+                        self.torch_rep.set_loss(loss)
+                    else:
+                        loss = tensors_mean(torch_output, "torch")
+                    if self.options["diff_phase"] == "both" or self.options["diff_phase"] == "backward":
+                        loss.backward()
+                except Exception as e:
+                    raise RuntimeError(
+                        "Exception is thrown while running forward of torch_module, please check the legality of module.\n{}".format(
+                            str(e)
+                        )
+                    )
+
+            with register_paddle_hooker(self.layer, self.layer_map):
+                try:
+                    paddle_output = self.layer(**paddle_input)
+                    if self.options["use_loss"]:
+                        loss = self.paddle_loss(paddle_output)
+                        self.paddle_rep.set_loss(loss)
+                    else:
+                        loss = tensors_mean(paddle_output, "paddle")
+                    if self.options["diff_phase"] == "both" or self.options["diff_phase"] == "backward":
+                        loss.backward()
+                except Exception as e:
+                    raise RuntimeError(
+                        "Exception is thrown while running forward of paddle_layer, please check the legality of layer.\n{}".format(
+                            str(e)
+                        )
+                    )
+
+        log("Max elementwise output diff is {}".format(max_diff(paddle_output, torch_output)))
